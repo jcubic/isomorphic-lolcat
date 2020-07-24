b98ee5e6fa14de5cd902140546b9990cd963fec4
Jakub Jankiewicz <jcubic@onet.pl>
2020-07-24 18:38:10 +0200

    all the files

---
+++ README.md
@@ -0,0 +1,86 @@
+# Isomorphic LolCat
+
+Small library based on [lolcatjs](https://github.com/robertmarsal/lolcatjs)
+that can be used in Node.js or Browser, to render gradient text.
+
+![Rainbow color ASCII art](https://github.com/jcubic/isomorphic-lolcat/blob/master/screenshot.png?raw=true)
+
+
+[Demo](http://jcubic.github.io/isomorphic-lolcat).
+
+## Installation
+
+```bash
+npm install isomorphic-lolcat
+```
+
+## Usage
+
+Node
+
+```javascript
+const lolcat = require('isomorphic-lolcat');
+```
+
+Browser
+
+```html
+<script src="https://unpkg.com/isomorphic-lolcat"></script>
+```
+
+## API
+
+* **lolcat.format(input: string, (char: string, color: color) => string, (line: number) => void): string[]**
+
+Low lovel function, first argument is function that is called on each character.
+First argument to function is single character second is object with `red`,
+`green` and `blue` properties of type number.
+
+Return array of string value is single lolcat string.
+
+### Example
+
+```javascript
+var styles = [];
+var i = 20;
+var o = rand(256);
+function eachLine() {
+  i -= 1;
+  lolcat.options.seed = o + i;
+}
+
+function rand(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+// render Style console in Browser
+console.log(lolcat.format(function(char, color) {
+    styles.push(`color: ${hex(color)}; background: black`);
+    return `%c${char}`;
+}, 'Lorem Ipsum Dolor Sit Amet', eachLine), ...styles);
+```
+
+* **lolcat.rainbow(input: string, (char: string, color: Color) => string): string[]**
+
+Shortcut using above example that use default seed for each line.
+
+* **lolcat.options**
+
+```javascript
+options: {
+    // Seed of the rainbow, use the same for the same pattern
+    seed: 0,
+    // Spread of the rainbow
+    spread: 8.0,
+    // Frequency of the rainbow colors
+    freq: 0.3,
+}
+```
+
+## License
+
+Copyright (C) 2020 [Jakub T. Jankiewicz](https://jcubic.pl) <jcubic@onet.pl>
+
+Copyright (C) 2015 [Robert Boloc](https://github.com/robertmarsal) <robertboloc@gmail.com>
+
+Released with WTFPL License
---
+++ index.js
@@ -0,0 +1,78 @@
+// ---------------------------------------------------------------------------------
+// Small Isomorphic lolcat library
+// Copyright (C) Jakub T. Jankiewicz
+// Copyright (C) Robert Boloc
+// Rleased under WTFPL License
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], function(BN) {
+            return (root.lolcat = factory());
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory();
+    } else {
+        root.lolcat = factory();
+    }
+})(typeof global !== 'undefined' ? global : self, function(undefined) {
+    var module = {
+        options: {
+            // Seed of the rainbow, use the same for the same pattern
+            seed: 0,
+            // Spread of the rainbow
+            spread: 8.0,
+            // Frequency of the rainbow colors
+            freq: 0.3,
+        }
+    };
+
+    var options = module.options;
+
+    var rainbow = function(freq, i) {
+
+        var red   = Math.round(Math.sin(freq * i + 0) * 127 + 128);
+        var green = Math.round(Math.sin(freq * i + 2 * Math.PI / 3) * 127 + 128);
+        var blue  = Math.round(Math.sin(freq * i + 4 * Math.PI / 3) * 127 + 128);
+
+        return {
+            red:   red,
+            green: green,
+            blue:  blue
+        }
+    };
+    
+    var printlnPlain = function(colorize, line) {
+        for (var i = 0; i < line.length; i++) {
+            colorize(line[i], rainbow(options.freq, options.seed + i / options.spread));
+        }
+    };
+    
+    module.rainbow = function render(string, fn) {
+        var i = 20;
+        var o = rand(256);
+        function eachLine() {
+          i -= 1;
+          lolcat.options.seed = o + i;
+        }
+        return lolcat.format(fn, string, eachLine);
+    };
+    
+    module.format = function format(string, format, onLine) {
+        var output = [];
+        var input = string.split('\n');
+        var line;
+        onLine = typeof onLine === 'function' ? onLine : function() {};
+        function buff(char, color) {
+            line.push(format(char, color));
+        }
+        for (var i = 0; i < input.length; ++i) {
+            line = [];
+            onLine(i);
+            printlnPlain(buff, input[i]);
+            output.push(line.join(''));
+        }
+        return output;
+    };
+    return module;
+});
